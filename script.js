45da5eff3cc81227a5f8aed37086c2671900d700
HeartOfAutumn <ngominhthuworks@gmail.com>
2020-05-15 22:54:57 +0700

    split style.css into smaller .css files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//Viết cho Slider
+var header = document.getElementById("owl-dots");
+var btns = header.getElementsByClassName("owl-dot");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+}
+
