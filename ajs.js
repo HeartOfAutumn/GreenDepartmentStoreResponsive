618990f1252826fe627e79c2b4391b83ccf6bd9f
HeartOfAutumn <ngominhthuworks@gmail.com>
2020-05-24 10:32:12 +0700

    Update

---
+++ ajs.js
@@ -0,0 +1,24 @@
+var ajs = document.getElementById("ajs-modal");
+var stickyfeedback = document.getElementById("stickyfeedback");
+var close = document.querySelector(".closePopupupContact");
+var menu = document.querySelector(".ocmenu");
+var colmenu = document.querySelector(".colmenu");
+var rowInColmenu = document.querySelector(".colmenu>span")
+
+stickyfeedback.addEventListener("click", function(){
+    ajs.style.display = "block";
+})
+
+close.addEventListener("click", function(){
+    ajs.style.display = "none";
+})
+
+menu.addEventListener("click", function(){
+    colmenu.style.display = "block";
+})
+
+rowInColmenu.addEventListener("click", function(){
+    colmenu.style.display = "none";
+})
+
+
